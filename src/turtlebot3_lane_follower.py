7260112e2cbdf6b0bdc694066dc4d1f3875ebadb
silvery107 <1079931505@qq.com>
2022-05-16 23:01:45 +0800

    update py node

--- src/turtlebot3_lane_follower.py
+++ src/turtlebot3_lane_follower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python3
 
 import rospy
 import cv2
@@ -24,10 +24,10 @@ rot_90 = np.array([0,1,-1,0], dtype=DTYPE).reshape((2,2))
 distCoeffs = np.array([0.135529, -0.197880, 0.009788, -0.003316, 0.000000], dtype=DTYPE)
 cameraMatrix = np.array([273.9783, 0.0, 151.81899, 0.0, 273.03021, 127.88242, 0.0, 0.0, 1.0],
                 dtype=DTYPE).reshape((3,3))
-top_x = 42 # 47
-top_y = 44 # 38
-bottom_x = 145
-bottom_y = 115 
+top_x = 68 # 47
+top_y = 20 # 38
+bottom_x = 155
+bottom_y = 116 
 # selecting 4 points from the original image
 pts_src = np.array([[160 - top_x, 180 - top_y], 
                     [160 + top_x, 180 - top_y], 
@@ -35,13 +35,14 @@ pts_src = np.array([[160 - top_x, 180 - top_y],
                     [160 - bottom_x, 120 + bottom_y]])
 
 # selecting 4 points from image that will be transformed
-pts_dst = np.array([[200, 0], [800, 0], [800, 600], [200, 600]])
+pts_dst = np.array([[64, 0], [256, 0], [256, 240], [64, 240]])
 # finding homography matrix
 H, status = cv2.findHomography(pts_src, pts_dst)
+H /= H[2,2]
 print(H)
 
 # Controllers and filters
-controller = NonholomonicController(0.029, 2.5, 0.5, max_v=0.22, max_w=2.84) # 0.031, 2.5, 0.5
+controller = NonholomonicController(0.01, 1.5, 0.5, max_v=0.22, max_w=2.84) # 0.031, 2.5, 0.5
 filter_x = MovingWindowFilter(1, dim=1)
 filter_y = MovingWindowFilter(1, dim=1)
 filter_t = MovingWindowFilter(2, dim=1)
@@ -62,7 +63,7 @@ def check_coord(coord):
 def get_lane_theta(mask):
     theta = 0.0
     mask_eroded = cv2.erode(mask, kernel, iterations=2)
-    contours, _ = cv2.findContours(mask_eroded, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask_eroded, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)[-2:]
     if contours:
         line_param = cv2.fitLine(contours[0], distType=cv2.DIST_L2, param=0, reps=0.01, aeps=0.01)
         new_coord = rot_90.dot(np.array([line_param[0], line_param[1]],dtype=DTYPE).reshape((2,1)))
@@ -115,13 +116,15 @@ class Follower:
             
 
         img_bird_view = cv2.warpPerspective(image, H, (image.shape[1], image.shape[0]))
+        # img_bird_view = image
         # cv2.imshow("BEV", img_bird_view)
 
         img_hsv = cv2.cvtColor(img_bird_view, cv2.COLOR_BGR2HSV)
-        h, w, d = image.shape
+        # cv2.imshow("HSV", img_hsv)
+        h, w, d = image.shape # (240, 320, 3)
 
         lower_black = np.array([0, 0, 0], dtype=DTYPE)
-        upper_black = np.array([180, 255, 46], dtype=DTYPE)
+        upper_black = np.array([180, 255, 70], dtype=DTYPE)
 
         mask1 = cv2.inRange(img_hsv[:,:int(w/2)], lower_black, upper_black)
         theta1 = get_lane_theta(mask1)
@@ -137,11 +140,13 @@ class Follower:
         mask2[0:search_top, 0:w] = 0
         # TODO 这里维护一下左右边线数组
         # Blind left and right 1/6
-        # mask1[:, 0:int(w/6)] = 0
-        # mask2[:, 5*int(w/6):] = 0
+        mask1[:, 0:int(w/6)] = 0
+        mask2[:, 2*int(w/6):] = 0
 
-        mask_add = mask1 + mask2
-        # cv2.imshow("masks", mask_add)
+        mask_add = np.zeros((h,w), dtype=DTYPE)
+        mask_add[:,:int(w/2)] = mask1
+        mask_add[:,int(w/2):] = mask2
+        cv2.imshow("masks", mask_add)
 
         M1 = cv2.moments(mask1)
         M2 = cv2.moments(mask2)
@@ -158,7 +163,7 @@ class Follower:
                 cx2 = 0
                 cy2 = 0
             else:
-                cx2 = int(M2['m10']/M2['m00'])
+                cx2 = int(M2['m10']/M2['m00']) + int(w/2)
                 cy2 = int(M2['m01']/M2['m00'])
 
             fpt_x = int((cx1 + cx2)/2)
@@ -178,8 +183,8 @@ class Follower:
             # print("dx:", dx)
             # print("dy:", dy)
             # print("theta:", rad2deg(theta))
-            # print("omega:", omega)
-            # print("veloc:", v)
+            print("omega:", omega)
+            print("veloc:", v)
 
             cv2.line(img_bird_view, (int(w/2-dy*5), h), (int(w/2-dy*5), h-int(dx*5)), (0, 0, 255), 2)
             cv2.line(img_bird_view, (int(w/2), h-2), (int(w/2-dy*5), h-2), (0, 0, 255), 2)
@@ -201,14 +206,14 @@ class Follower:
                     self.crossing_counter = 0
                     print("Refresh Stop State")
 
-            self.twist.linear.x = v
+            self.twist.linear.x = 0.22#v
             self.twist.angular.z = omega
 
             self.cmd_vel_pub.publish(self.twist)
         
         img_pair = np.concatenate((image, img_bird_view), axis=1)
         cv2.imshow("image", img_pair)
-        cv2.waitKey(27)
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
     rospy.init_node('lane_follower')
